583649b1c92fb82a0275c5d8238285567cc57f00
woyiswoy <woypatcharapon@gmail.com>
2025-06-16 23:29:09 +0700

    adding cloudflare analytics

---
+++ cf-analytics.js
@@ -0,0 +1,7 @@
+(function () {
+  var script = document.createElement('script');
+  script.defer = true;
+  script.src = 'https://static.cloudflareinsights.com/beacon.min.js';
+  script.setAttribute('data-cf-beacon', '{"token": "e7f7277f701147f9a8414e59ee22e10c"}');
+  document.head.appendChild(script);
+})();
